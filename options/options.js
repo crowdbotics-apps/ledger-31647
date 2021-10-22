d7edd93697c1601d47c0066b2ecdfb7c0142da8a
Crowdbotics <team@crowdbotics.com>
2021-10-22 16:48:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ledger_31647",
+  url: "https://ledger_31647.botics.co",
+  api: "https://ledger_31647.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
